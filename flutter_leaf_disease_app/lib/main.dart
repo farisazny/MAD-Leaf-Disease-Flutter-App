20ba5baf60f0abe85acab15262894fd410c2db29
ihz16 <84853772+ihz16@users.noreply.github.com>
2024-01-17 17:02:47 +0800

    main2.dart

--- flutter_leaf_disease_app/lib/main.dart
+++ flutter_leaf_disease_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
@@ -27,14 +28,16 @@ class _LeafDiseasePredictorState extends State<LeafDiseasePredictor> {
   String _prediction = "Prediction will appear here";
 
   Future<Map<String, dynamic>> detectDisease(String imagePath) async {
-    final String apiUrl = 'http://10.0.2.2:5000/predict'; // Replace with your Flask API endpoint
+    final String apiUrl =
+        "https://flask-production-4847.up.railway.app/predict"; // Replace with your Flask API endpoint
 
     try {
       var request = http.MultipartRequest('POST', Uri.parse(apiUrl))
         ..files.add(await http.MultipartFile.fromPath('img', imagePath));
 
       final response = await request.send();
-      final Map<String, dynamic> data = json.decode(await response.stream.bytesToString());
+      final Map<String, dynamic> data =
+          json.decode(await response.stream.bytesToString());
 
       return data;
     } catch (e) {
@@ -44,7 +47,8 @@ class _LeafDiseasePredictorState extends State<LeafDiseasePredictor> {
 
   Future<void> _testConnection() async {
     // Replace the API endpoint with your Flask API endpoint
-    const testConnectionUrl = "http://10.0.2.2:5000/testConnection";
+    const testConnectionUrl =
+        "https://flask-production-4847.up.railway.app/testConnection";
 
     try {
       final response = await http.get(Uri.parse(testConnectionUrl));
@@ -68,23 +72,50 @@ class _LeafDiseasePredictorState extends State<LeafDiseasePredictor> {
     }
   }
 
-Future<void> _predictLeafDisease() async {
-  if (_image == null) {
-    return;
+// Function to download an image from a URL
+  Future<void> fetchImage(String imageUrl) async {
+    try {
+      final response = await http.get(Uri.parse(imageUrl));
+
+      if (response.statusCode == 200) {
+        final documentDirectory = await getApplicationDocumentsDirectory();
+        final file = File('${documentDirectory.path}/temp_image.jpg');
+        file.writeAsBytesSync(response.bodyBytes);
+
+        setState(() {
+          _image = file; // Update _image to display the downloaded image
+        });
+
+        // Now you can use this image for prediction
+        await detectDisease(file.path);
+      } else {
+        // Handle server response error
+        print('Failed to download the image: Server error');
+      }
+    } catch (e) {
+      // Handle any other type of error
+      print('Failed to download the image: $e');
+    }
   }
 
-  try {
-    final result = await detectDisease(_image!.path);
+  Future<void> _predictLeafDisease() async {
+    if (_image == null) {
+      return;
+    }
 
-    setState(() {
-      final accuracy = result['Accuracy'];
-      _prediction = "Prediction: ${result['Label Name']} (Accuracy: ${accuracy != null ? accuracy.toStringAsFixed(2) : 'N/A'})";
-    });
-  } catch (e) {
-    // Handle errors
-    print(e.toString());
+    try {
+      final result = await detectDisease(_image!.path);
+
+      setState(() {
+        final accuracy = result['Accuracy'];
+        _prediction =
+            "Prediction: ${result['Label Name']} (Accuracy: ${accuracy != null ? accuracy.toStringAsFixed(2) : 'N/A'})";
+      });
+    } catch (e) {
+      // Handle errors
+      print(e.toString());
+    }
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -112,6 +143,11 @@ Future<void> _predictLeafDisease() async {
             ),
             const SizedBox(height: 20),
             ElevatedButton(
+              onPressed: () => fetchImage(
+                  "https://www.planetnatural.com/wp-content/uploads/2012/12/anthracnose-1.jpg"),
+              child: Text('Fetch Example Image'),
+            ),
+            ElevatedButton(
               onPressed: _predictLeafDisease,
               child: const Text("Predict Disease"),
             ),
@@ -126,3 +162,4 @@ Future<void> _predictLeafDisease() async {
     );
   }
 }
+
